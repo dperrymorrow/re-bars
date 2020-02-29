c8336360aa4bae34db730412aefd2763949897a6
David Morro <dperrymorrow@gmail.com>
2020-02-28 16:40:50 -0800

    append

--- src/helpers/re-renders.js
+++ src/helpers/re-renders.js
@@ -1,34 +1,38 @@
 import Utils from "../utils.js";
 
 export default function(storage, { instance, data }) {
-  const _watch = (target, render) => {
-    const eId = Utils.randomId();
-    if (target === null)
-      throw new Error("Rebars: cannot pass null to watch helper, pass a string or Object");
-    const path = typeof target === "object" ? `${target.ReBarsPath}.*` : target.split(",");
+  const _getPath = (target, wildcard = true) => {
+    return typeof target === "object"
+      ? `${target.ReBarsPath}${wildcard ? ".*" : ""}`
+      : target.split(",");
+  };
 
+  const _watch = (path, render) => {
+    const eId = Utils.randomId();
     storage.renders[eId] = { render, path };
     return eId;
   };
 
   instance.registerHelper("debug", function(obj) {
     const render = () => `<pre class="debug">${JSON.stringify(obj, null, 2)}</pre>`;
-    const eId = _watch(obj, render);
+    const eId = _watch(_getPath(obj), render);
     return new instance.SafeString(Utils.wrapTemplate(eId, render()));
   });
 
-  instance.registerHelper("watch", function(target, { fn }) {
-    const eId = _watch(target, fn.bind(null, data));
-    return Utils.wrapTemplate(eId, fn(data));
+  instance.registerHelper("watch", function(...args) {
+    const { fn } = args.pop();
+    const path = args.map(arg => _getPath(arg, false)).join(".");
+    const eId = _watch(path, () => fn(this, data));
+    return Utils.wrapTemplate(eId, fn(this));
   });
 
-  instance.registerHelper("watchEach", function(arr, { fn }) {
-    if (!Array.isArray(arr)) throw new Error("watchEach must be passed an Array");
-    const path = arr.ReBarsPath;
-
-    return arr.map((item, index) => {
-      const eId = _watch(`${path}.${index}.*`, fn.bind(null, item));
-      return Utils.wrapTemplate(eId, fn(item));
-    });
-  });
+  // instance.registerHelper("watchEach", function(arr, { fn }) {
+  //   if (!Array.isArray(arr)) throw new Error("watchEach must be passed an Array");
+  //   const path = arr.ReBarsPath;
+  //
+  //   return arr.map(function(item, index) {
+  //     const eId = _watch(`${path}.${index}.*`, fn.bind(this));
+  //     return Utils.wrapTemplate(eId, fn(this));
+  //   });
+  // });
 }
