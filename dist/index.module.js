34a860b69d0086b2751e46338bfc53ce9ca300a3
David Morro <dperrymorrow@gmail.com>
2020-04-01 16:25:09 -0700

    docs finished

--- dist/index.module.js
+++ dist/index.module.js
@@ -35,7 +35,8 @@ const messages = {
   tplStr: ({ name }) => `component:${name} needs a template string`,
   propStomp: ({ name, key }) => `component:${name} "data.${key}" was overrode by a prop`,
   propUndef: ({ name, key }) => `component:${name} was passed undefined for prop "${key}"`,
-  oneRoot: ({ name }) => `component:${name} must have one root node, and cannot be a {{#watch}} block`,
+  oneRoot: ({ name }) =>
+    `component:${name} must have one root node, and cannot be a {{#watch}} block. \nThis error can also be caused by malformed html.`,
   noMethod: ({ name, methodName }) => `component:${name} does not have a method named "${methodName}"`,
   badPath: ({ path }) => `${path} was not found in object`,
   reRender: ({ name, path }) => `component:${name} re-rendering "${path}"`,
