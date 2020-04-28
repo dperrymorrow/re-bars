445bdb673a443fc02ec7af48a28effbb12355b52
David Morro <dperrymorrow@gmail.com>
2020-04-28 15:37:02 -0700

    watch specs

--- test/specs/helpers/watch.spec.js
+++ test/specs/helpers/watch.spec.js
@@ -19,6 +19,18 @@ test("throws if value undefined", async t => {
   t.true(message.includes(root.template));
 });
 
+test("does not throw if passed a string", async t => {
+  Helpers.buildContext(t, {
+    root: {
+      name: "test",
+      template: "<div>{{#watch 'some.*.crazy.*.path' }}{{/watch}}</div>",
+    },
+  });
+
+  const { path } = Object.values(t.context.inst.renders)[0];
+  t.deepEqual(path, ["some.*.crazy.*.path"]);
+});
+
 test("builds the dom $el", async t => {
   Helpers.buildContext(t, {
     root: {
@@ -48,4 +60,43 @@ test("takes a tag, and or optional attrs ", async t => {
 
   t.is($watch.nodeName, "DIV");
   t.is($watch.getAttribute("foo"), "bar");
+});
+
+test("can watch multiple items", async t => {
+  Helpers.buildContext(t, {
+    root: {
+      name: "test",
+      template: "<div>{{#watch 'name,hobby' }}{{/watch}}</div>",
+      data: () => ({ name: "Dave", hobby: "bonsai" }),
+    },
+  });
+
+  const { path } = Object.values(t.context.inst.renders)[0];
+  t.deepEqual(path, ["name", "hobby"]);
+});
+
+test("uses wildcard if an Object", async t => {
+  Helpers.buildContext(t, {
+    root: {
+      name: "test",
+      template: "<div>{{#watch name }}{{/watch}}</div>",
+      data: () => ({ name: { first: "Dave", last: "Morrow" } }),
+    },
+  });
+
+  const { path } = Object.values(t.context.inst.renders)[0];
+  t.deepEqual(path, ["name.*"]);
+});
+
+test("uses wildcard on Index of Array", async t => {
+  Helpers.buildContext(t, {
+    root: {
+      name: "test",
+      template: "<div>{{#watch 'friends.*.name' }}{{/watch}}</div>",
+      data: () => ({ friends: ["Mike", "Jason"] }),
+    },
+  });
+
+  const { path } = Object.values(t.context.inst.renders)[0];
+  t.deepEqual(path, ["friends.*.name"]);
 });
